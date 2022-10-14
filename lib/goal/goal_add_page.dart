755ab9f69a80f8b41e66c37d382590bec09f60f2
Haeseong Jeon <seong0428@gmail.com>
2022-10-15 03:53:01 +0900

    feat: 목표 추가 페이지 레이아웃 적용

--- lib/goal/goal_add_page.dart
+++ lib/goal/goal_add_page.dart
@@ -8,40 +8,209 @@ class GoalAddPage extends StatefulWidget {
 }
 
 class _GoalAddPageState extends State<GoalAddPage> {
+  bool dateRangeEnabled = false;
+  bool timeRangeEnabled = false;
+
+  /// TODO: textField 현재글자수 표시, 글자수 제한
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Column(
+          children: [
+            /// 닫기 버튼
+            Align(
+              alignment: Alignment.centerRight,
+              child: IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const Icon(Icons.close),
+              ),
+            ),
+            const SizedBox(
+              height: 7,
+            ),
+            Expanded(
+              child: _getFormWidgets(),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 20.0,
+                right: 20.0,
+                bottom: 20.0,
+              ),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                ),
+                child: const Text(
+                  '목표 만들기',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _getFormWidgets() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 20.0,
+        right: 20.0,
+        bottom: 20.0,
+      ),
+      child: SingleChildScrollView(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('목표 생성하기'),
-            const Text('목표명'),
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: '최대 20자',
+            const Text(
+              '짝심목표 만들기',
+              style: TextStyle(
+                fontSize: 24,
               ),
             ),
-            const Text('기간'),
-            const Text('2022-01-01 ~ 2022-12-31'),
-            const Text('수행시간'),
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: '최대 20자',
+            const SizedBox(
+              height: 35,
+            ),
+
+            /// 목표
+            const Text(
+              '목표',
+              style: TextStyle(
+                fontSize: 15,
               ),
             ),
-            const Text('Push 알림 설정'),
             TextFormField(
               decoration: const InputDecoration(
-                hintText: '푸쉬알림 내용',
+                hintText: '짝심목표를 알려주세요',
               ),
             ),
-            const Spacer(),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('저장'),
+            const SizedBox(height: 25),
+
+            /// 목표 기간
+            Row(
+              children: [
+                const Text(
+                  '목표 기간',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                const Spacer(),
+                Switch(
+                  value: dateRangeEnabled,
+                  onChanged: (value) {
+                    setState(() {
+                      dateRangeEnabled = value;
+                    });
+                  },
+                )
+              ],
+            ),
+            // TODO: 시작, 종료 시각 사이 선 긋기
+            Visibility(
+              visible: dateRangeEnabled,
+              child: Column(
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(10.0),
+                      ),
+                      color: Color.fromRGBO(0xF9, 0xFa, 0xFB, 1.0),
+                    ),
+                    height: 45,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        children: const [
+                          Text('시작'),
+                          Spacer(),
+                          Text('2022. 10. 13.'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.vertical(
+                        bottom: Radius.circular(10.0),
+                      ),
+                      color: Color.fromRGBO(0xF9, 0xFa, 0xFB, 1.0),
+                    ),
+                    height: 45,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        children: const [
+                          Text('종료'),
+                          Spacer(),
+                          Text('2022. 10. 13.'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                ],
+              ),
+            ),
+
+
+            /// 수행시간
+            Row(
+              children: [
+                const Text(
+                  '수행시간',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                const Spacer(),
+                Switch(
+                  value: timeRangeEnabled,
+                  onChanged: (value) {
+                    setState(() {
+                      timeRangeEnabled = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            Visibility(
+              visible: timeRangeEnabled,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  hintText: '시간을 선택해주세요',
+                ),
+              ),
+            ),
+            const SizedBox(height: 25),
+
+            /// Push 알림 설정
+            const Text(
+              'Push 알림 설정',
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: '시간을 선택해주세요',
+              ),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Push 알림 내용을 입력해주세요',
+              ),
             ),
           ],
         ),
